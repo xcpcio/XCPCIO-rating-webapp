74ba6e88976d94bf4e69ef0076bf21d4c2d680b9
Dup4 <lyuzhi.pan@gmail.com>
2021-05-30 17:38:53 +0800

    feat: 6th ccpc final

--- config.ts
+++ config.ts
@@ -18,7 +18,8 @@ export const config = {
     ],
     proxy: {},
     dataHost:
-        'https://cdn.jsdelivr.net/gh/XCPCIO/XCPCIO-rating-data@2021.5.17/',
+        // 'https://raw.githubusercontent.com/XCPCIO/XCPCIO-rating-data/gh-pages/',
+        'https://cdn.jsdelivr.net/gh/XCPCIO/XCPCIO-rating-data@2021.5.31.1/',
 };
 
 export default config;
